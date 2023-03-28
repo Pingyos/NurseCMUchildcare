32494ea2a956b8226756814253da80e37934812e
loveopo1999@gmail.com <loveopo1999@gmail.com>
2023-03-28 16:09:39 +0700

    update

--- Add_Admin/production/dashboard.php
+++ Add_Admin/production/dashboard.php
@@ -1,49 +1,29 @@
-<?php require_once 'head.php'; ?>
+<?php require_once 'head.php';
+
+session_start();
+require_once 'connection.php';
+if (!isset($_SESSION['admin_login'])) {
+  $_SESSION['error'] = 'กรุณาเข้าสู่ระบบ!';
+  header('location: index.php');
+}
+
+?>
 
 <body class="nav-md">
   <div class="container body">
     <div class="main_container">
-    <?php require_once 'sidebar.php'; ?>
+      <?php require_once 'sidebar.php';
+      require_once 'topbar.php';
+      ?>
       <!-- page content -->
+
       <div class="right_col" role="main">
         <div class="row" style="display: inline-block;">
-          <div class=" top_tiles" style="margin: 10px 0;">
-            <div class="col-md-3 col-sm-3  tile">
-              <span>Total Sessions</span>
-              <h2>231,809</h2>
-              <span class="sparkline_one" style="height: 160px;">
-                <canvas width="200" height="60" style="display: inline-block; vertical-align: top; width: 94px; height: 30px;"></canvas>
-              </span>
-            </div>
-            <div class="col-md-3 col-sm-3  tile">
-              <span>Total Revenue</span>
-              <h2>$ 231,809</h2>
-              <span class="sparkline_one" style="height: 160px;">
-                <canvas width="200" height="60" style="display: inline-block; vertical-align: top; width: 94px; height: 30px;"></canvas>
-              </span>
-            </div>
-            <div class="col-md-3 col-sm-3  tile">
-              <span>Total Sessions</span>
-              <h2>231,809</h2>
-              <span class="sparkline_one" style="height: 160px;">
-                <canvas width="200" height="60" style="display: inline-block; vertical-align: top; width: 94px; height: 125px;"></canvas>
-              </span>
-            </div>
-            <div class="col-md-3 col-sm-3  tile">
-              <span>Total Sessions</span>
-              <h2>231,809</h2>
-              <span class="sparkline_one" style="height: 160px;">
-                <canvas width="200" height="60" style="display: inline-block; vertical-align: top; width: 94px; height: 30px;"></canvas>
-              </span>
-            </div>
-          </div>
-        </div>
-        <br />
 
+        </div>
       </div>
+      <!-- /page content -->
     </div>
-    <!-- /page content -->
-  </div>
   </div>
   <?php require_once 'script.php'; ?>
 </body>
